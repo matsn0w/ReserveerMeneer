fb7bbceae4e8a50e4126dee37312997c9d661741
Niels van Hal <ntj.vanhal@student.avans.nl>
2021-03-02 11:52:13 +0100

    Restaurant Model and Database setup

---
+++ database/migrations/2021_03_02_105006_create_restaurant_openinghours_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantOpeninghoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurant_openinghours', function (Blueprint $table) {
+            $table->id();
+            $table->string('weekday');
+            $table->timestamp('openingtime');
+            $table->timestamp('closingtime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurant_openinghours');
+    }
+}
