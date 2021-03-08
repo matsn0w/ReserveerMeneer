76d3e83a6ccf70603f2a9c9c28587bd66cb469fc
Niels van Hal <ntj.vanhal@student.avans.nl>
2021-03-08 15:53:36 +0100

    Added default crud for events

---
+++ resources/views/events/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base', [
+    'title' => $event->name
+])
+
+@section('content')
+    <p>
+        <strong>Beschrijving:</strong> {{ $event->description }}<br>
+        <strong>Start datum:</strong> {{ $event->startdate }}<br>
+        <strong>Eind datum:</strong> {{ $event->enddate }}<br>
+        <strong>Maximaal aantal tickets per persoon:</strong> {{ $event->maxPerPerson }}</a>
+    </p>
+
+    <div class="block">
+        <a href="{{ route('events.edit', $event) }}">Bewerken</a> |
+        <a href="{{ route('events.index') }}">Terug naar overzicht</a>
+    </div>
+@endsection
