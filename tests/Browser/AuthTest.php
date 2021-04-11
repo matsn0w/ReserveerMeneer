610a8c61e133e00553c9a3cc2b214dc1926fd325
matsn0w <bartscholtus@gmail.com>
2021-04-11 23:18:31 +0200

    auth tests

--- tests/Browser/AuthTest.php
+++ tests/Browser/AuthTest.php
@@ -5,12 +5,45 @@ namespace Tests\Browser;
 use App\Models\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class AuthTest extends DuskTestCase
 {
     use DatabaseMigrations;
+
+    protected static $migrationRun = false;
+
+    public function setUp(): void{
+        parent::setUp();
+
+        if (!static::$migrationRun){
+            $this->artisan('migrate:fresh --seed');
+            static::$migrationRun = true;
+        }
+    }
+
+    /**
+     * Tests if a user is abled to register.
+     *
+     * @return void
+     */
+    public function test_register()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('register')
+                ->type('name', 'Tedje Test')
+                ->type('email', 'tedje@test.com')
+                ->type('phonenumber', '31612345678')
+                ->type('password', 'Password123!')
+                ->type('password_confirmation', 'Password123!')
+                ->press('Registreer')
+                ->assertPathIs('/')
+                ->assertSee('Je bent ingelogd als Tedje Test')
+                ->logout();
+        });
+    }
 
     /**
      * Tests if a user is abled to log in.
@@ -19,16 +52,16 @@ class AuthTest extends DuskTestCase
      */
     public function test_login()
     {
-        $user = User::factory()->create([
-            'email' => 'test@laravel.dusk',
-            'password' => Hash::make('Password123!'),
-            'phonenumber' => '1234'
-        ]);
+        $user = User::factory([
+            'name' => 'Teddie',
+            'email' => 'teddie@hotmail.nl',
+            'phonenumber' => '123' // TODO: this can be removed
+        ])->create();
 
         $this->browse(function (Browser $browser) use ($user) {
             $browser->visitRoute('login')
                 ->type('email', $user->email)
-                ->type('password', 'Password123!')
+                ->type('password', 'password')
                 ->press('Inloggen')
                 ->assertPathIs('/');
         });
@@ -42,9 +75,52 @@ class AuthTest extends DuskTestCase
     public function test_logout()
     {
         $this->browse(function (Browser $browser) {
-            $browser->assertSee('Log uit')
+            $user = User::factory([
+                'name' => 'Teddie',
+                'email' => 'teddie@hotmail.nl',
+                'phonenumber' => '123' // TODO: this can be removed
+            ])->create();
+
+            $browser->loginAs(User::find(1))
+                ->visitRoute('home')
+                ->assertSee('Log uit')
                 ->clickLink('Log uit')
                 ->assertPathIs('/');
+        });
+    }
+
+    /**
+     * Tests if a user is denied to see all movies.
+     *
+     * @return void
+     */
+    public function test_access_denied()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visitRoute('movies.index')
+                ->assertSee('403')
+                ->assertPathIs('/movies');
+        });
+    }
+
+    /**
+     * Tests if a user is allowed to see all film events.
+     *
+     * @return void
+     */
+    public function test_access_granted()
+    {
+        $user = User::factory([
+            'name' => 'Teddie',
+            'email' => 'teddie@hotmail.nl',
+            'phonenumber' => '123' // TODO: this can be removed
+        ])->create();
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                ->visitRoute('filmevents.index')
+                ->assertSee('Filmavonden')
+                ->assertPathIs('/filmevents');
         });
     }
 }
