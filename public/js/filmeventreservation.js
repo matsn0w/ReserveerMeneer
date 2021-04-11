5c7cd9cdd51ed218129d707812a15d3efbb90242
matsn0w <bartscholtus@gmail.com>
2021-04-11 12:55:55 +0200

    disable/enable seats to the left and right

--- public/js/filmeventreservation.js
+++ public/js/filmeventreservation.js
@@ -15,6 +15,10 @@ var __webpack_exports__ = {};
   var seats = document.querySelectorAll('.seat');
   seats.forEach(function (seat) {
     seat.addEventListener('click', function (e) {
+      if (seat.disabled) {
+        return;
+      }
+
       toggleSeat(seat);
     });
   });
@@ -31,6 +35,38 @@ var __webpack_exports__ = {};
     }
 
     input.value = seatIDs;
+    toggleLeftSeat(seat);
+    toggleRightSeat(seat);
+  };
+
+  var toggleLeftSeat = function toggleLeftSeat(seat) {
+    var leftID = parseInt(seat.getAttribute('data-seat-id')) - 1;
+    var leftSeat = document.querySelector(".seat[data-seat-id=\"".concat(leftID, "\"]"));
+
+    if (leftSeat == null) {
+      return;
+    } // check if the left seat is in the same row
+
+
+    if (leftSeat.parentElement === seat.parentElement) {
+      leftSeat.disabled = !leftSeat.disabled;
+      leftSeat.classList.toggle('disabled');
+    }
+  };
+
+  var toggleRightSeat = function toggleRightSeat(seat) {
+    var rightID = parseInt(seat.getAttribute('data-seat-id')) + 1;
+    var rightSeat = document.querySelector(".seat[data-seat-id=\"".concat(rightID, "\"]"));
+
+    if (rightSeat == null) {
+      return;
+    } // check if the right seat is in the same row
+
+
+    if (rightSeat.parentElement === seat.parentElement) {
+      rightSeat.disabled = !rightSeat.disabled;
+      rightSeat.classList.toggle('disabled');
+    }
   };
 })();
 /******/ })()
--- resources/js/filmeventreservation.js
+++ resources/js/filmeventreservation.js
@@ -11,6 +11,10 @@
 
     seats.forEach(seat => {
         seat.addEventListener('click', (e) => {
+            if (seat.disabled) {
+                return;
+            }
+
             toggleSeat(seat);
         });
     });
@@ -29,5 +33,38 @@
         }
 
         input.value = seatIDs;
+
+        toggleLeftSeat(seat);
+        toggleRightSeat(seat);
+    };
+
+    const toggleLeftSeat = (seat) => {
+        let leftID = parseInt(seat.getAttribute('data-seat-id')) - 1;
+        let leftSeat = document.querySelector(`.seat[data-seat-id="${leftID}"]`);
+
+        if (leftSeat == null) {
+            return;
+        }
+
+        // check if the left seat is in the same row
+        if (leftSeat.parentElement === seat.parentElement) {
+            leftSeat.disabled = !leftSeat.disabled;
+            leftSeat.classList.toggle('disabled');
+        }
+    };
+
+    const toggleRightSeat = (seat) => {
+        let rightID = parseInt(seat.getAttribute('data-seat-id')) + 1;
+        let rightSeat = document.querySelector(`.seat[data-seat-id="${rightID}"]`);
+
+        if (rightSeat == null) {
+            return;
+        }
+
+        // check if the right seat is in the same row
+        if (rightSeat.parentElement === seat.parentElement) {
+            rightSeat.disabled = !rightSeat.disabled;
+            rightSeat.classList.toggle('disabled');
+        }
     };
 })();
